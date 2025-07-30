0e58cce7733bc049ced13ff25f078753bf3573f5
RohitPatil1857 <rrpatil615@gmail.com>
2025-07-30 11:21:16 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,12 +1,12 @@
-def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeAPI}"){
-      sh """
-         mvn clean install
-         ${SONAR_HOME}/bin/sonar-scanner \
-         -Dsonar.projectName=${Projectname} \
-         -Dsonar.projectKey=${ProjectKey} \
-         -Dsonar.java.binaries=target \
-         -X
-      """
+def call(String SonarQubeAPI, String Projectname, String ProjectKey) {
+  withSonarQubeEnv("${SonarQubeAPI}") {
+    sh """
+      mvn clean install -DskipTests
+      ${SONAR_HOME}/bin/sonar-scanner \
+        -Dsonar.projectName=${Projectname} \
+        -Dsonar.projectKey=${ProjectKey} \
+        -Dsonar.java.binaries=target \
+        -X
+    """
   }
 }
